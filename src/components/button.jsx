ff2afe1254313f85032e6a340f89317d298d5c30
Chathur0 <145315287+Chathur0@users.noreply.github.com>
2024-05-21 15:30:18 +0530

    complete home page

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -6,7 +6,7 @@ export default function button(props) {
       <button
         type="button"
         className="btn btn-custom"
-        style={{ display: "none" }}
+        style={{ display: `${props.displayType}`,paddingTop: "8px",paddingBottom: "8px" }}
       >
         {props.content}
       </button>
